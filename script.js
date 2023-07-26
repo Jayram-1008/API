fea5d73c0e1e0b6b4d3f84ffad35ca0f02566acd
Jayram Kumar <jayram14032001@gmail.com>
2023-07-26 19:03:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+class User{
+    constructor(email, password){
+        
+        
+        this.password = password
+        console.log("Constructor")
+        this.email = email;
+       
+    }
+    get email(){
+        
+        return `${this._email}jayram`
+    }
+    set email(value){
+        console.log("Set")
+        this._email = value;
+    }
+    // get password(){
+        
+    //     return `${this._password}jayram`
+    // }
+    // set password(value){
+    //     console.log("Set")
+    //     this._password = value;
+    // }
+}
+
+const hay = new User("jay@gmail.com", "abc")
+console.log(hay.email);
